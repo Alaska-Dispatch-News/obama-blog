917a203a267572b4e4b23396c49618599c289852
James Ives <jives@alaskadispatch.com>
2015-08-21 17:18:19 -0800

    Initial Commit
    Starting up the repo with Yeoman scaffolding.
    
    * Used Yeoman AngularJS generator.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name obamaBlogApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the obamaBlogApp
+ */
+angular.module('obamaBlogApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

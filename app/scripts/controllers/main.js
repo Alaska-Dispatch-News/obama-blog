72331f0c3d989a9d9bf501036fec7c3ee5edd43f
James Ives <iam@jamesiv.es>
2015-08-23 14:25:05 -0800

    Feed Implementation
    $http calls have been implemented in this push along with additional
    markup changes. I’m pulling the feed in remotely currently as I don’t
    have a working dev environment.
    
    * $http has been added and binds the data to the DOM using
    ng-trusted-html.
    * Adjusted the markup so it’s more consistent.
    * Added styles to allow images pulled from JSON to be responsive.
    * Added ng-cloak to prevent page flash.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the obamaBlogApp
  */
 angular.module('obamaBlogApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http, $sce) {
+
+    $scope.to_trusted = function(html_code) {
+      return $sce.trustAsHtml(html_code)
+    };
+    // Pulls in the json object found in the data directory.
+      $http.get('http://www.adn.com/appdata/views/ad_app_feed/?display_id=arctic_section_front&offset=0&limit=14&args[0]=18&args[1]=18.json')
+        .success(function(data, status, headers, config) {
+        $scope.articles = data;
+
+      })
+        .error(function(data, status, headers, config) {
+        console.log("Error Receiving Data");
+      });
   });

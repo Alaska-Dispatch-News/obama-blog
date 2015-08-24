5b7311eb79f250d36533cfb7c3f3421c911fa148
James Ives <iam@jamesiv.es>
2015-08-23 22:24:43 -0800

    Comments & Cleanup
    * Removed a bunch of un-needed code.
    * Cleaned up some of the CSS by extending a number of Bootstrap classes.
    * Added code comments.
    * Added meta data.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,16 +10,19 @@
 angular.module('obamaBlogApp')
   .controller('MainCtrl', function ($scope, $http, $sce) {
 
+    /* Creates a re-usable function that can be used to bind HTML to the DOM from
+      the JSON. */
     $scope.to_trusted = function(html_code) {
       return $sce.trustAsHtml(html_code)
     };
-    // Pulls in the json object found in the data directory.
-      $http.get('http://www.adn.com/appdata/views/ad_app_feed/?display_id=arctic_section_front&offset=0&limit=14&args[0]=18&args[1]=18.json')
-        .success(function(data, status, headers, config) {
-        $scope.articles = data;
+    /* Pulls in JSON and binds it to $scope.articles. */
+    $http.get('http://www.adn.com/appdata/views/ad_app_feed/?display_id=arctic_section_front&offset=0&limit=14&args[0]=18&args[1]=18.json')
+      .success(function(data) {
+      $scope.articles = data;
 
-      })
-        .error(function(data, status, headers, config) {
-        console.log("Error Receiving Data");
-      });
+    })
+    /* If no content is retrieved it will display a console error. */
+      .error(function(data) {
+      console.log("Error Receiving Data");
+    });
   });

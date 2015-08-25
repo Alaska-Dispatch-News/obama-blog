d857301bdcfd41e7e285db12423a98a5d9a2df29
James Ives <iam@jamesiv.es>
2015-08-24 17:51:34 -0800

    Updated Design
    Site is now ready for advert addition.
    
    * Added media queries for the h1 tags within the blog-post. Reduces the
    size to prevent overflow.
    * Reduced padding for small devices.
    * Added the $alaska-yellow variable.
    * Split up the SCSS files into individual files to keep things more
    organized.
    * Removed unused images.
    * Finalized column placements.
    * Added copyright icon to the footer.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,23 +29,36 @@ angular
         redirectTo: '/'
       });
   }).factory('Article', function($http) {
+  /* Article Factory: Pulls the API data from the JSON object and allows
+  the page to scroll until it reaches the end of the file. Used to save bandwidth
+  and to create an endless scroll effect. */
   var Article = function() {
     this.items = [];
     this.busy = false;
     this.after = '';
   };
 
+  /* Prototype sets up the next page function used for the infinite scrolling. */
   Article.prototype.nextPage = function() {
     if (this.busy) return;
     this.busy = true;
 
+    /* Sets up the URL to the JSON. Replaces the offset with this.after */
     var url = "http://www.adn.com/appdata/views/ad_app_feed/?display_id=arctic_section_front&offset=" + this.after + "&limit=3&args[0]=18&args[1]=18.json"
     $http.get(url).success(function(data) {
       //console.log("Succesfully called API");
+
+      /* Assigns the succesful JSON call to the variable items. */
       var items = data;
+
+      /* Loops through the items variable and pushes them to the this.items array. */
       for (var i = 0; i < items.length; i++) {
         this.items.push(items[i]);
       }
+
+      /* Assigns this.after to the amount of items in the array plus three. The feed pulls in
+      three articles at a time. The next time it's run it will offset it by three to prevent
+      the same articles from being pulled again. */
       this.after = this.items.length + 3;
       this.busy = false;
     }.bind(this));
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,15 +15,7 @@ angular.module('obamaBlogApp')
     $scope.to_trusted = function(html_code) {
       return $sce.trustAsHtml(html_code)
     };
-    /* Pulls in JSON and binds it to $scope.articles. */
-    $http.get('http://www.adn.com/appdata/views/ad_app_feed/?display_id=arctic_section_front&offset=0&limit=14&args[0]=18&args[1]=18.json')
-      .success(function(data) {
-      $scope.articles = data;
 
-    })
-    /* If no content is retrieved it will display a console error. */
-      .error(function(data) {
-      console.log("Error Receiving Data");
-    });
+    /* Creates a new article object. Used alongside the Article factory. */
     $scope.article = new Article();
   });

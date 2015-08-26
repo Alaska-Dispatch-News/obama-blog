0195784571fefa2ea006b1d51002803528e26a56
James Ives <iam@jamesiv.es>
2015-08-25 16:12:04 -0800

    Production
    Preparing files for production.
    
    * Added size property to post images.
    * Added Angular-Filter support. Set the articles to filter by the node
    ID to prevent duplicate articles.
    * Made some adjustments so the article offsets do not cause problems.
    * Updated README file.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'infinite-scroll'
+    'infinite-scroll',
+    'angular.filter'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -35,18 +36,21 @@ angular
   var Article = function() {
     this.items = [];
     this.busy = false;
-    this.after = '';
+    this.after = 0;
   };
 
   /* Prototype sets up the next page function used for the infinite scrolling. */
   Article.prototype.nextPage = function() {
-    if (this.busy) return;
+    if (this.busy) {
+      return;
+    }
     this.busy = true;
-
+    console.log(this.after);
     /* Sets up the URL to the JSON. Replaces the offset with this.after */
-    var url = "http://www.adn.com/appdata/views/ad_app_feed/?display_id=arctic_section_front&offset=" + this.after + "&limit=3&args[0]=18&args[1]=18.json"
+    var url = "http://www.adn.com/appdata/views/ad_app_feed/?display_id=section_front&offset=" + this.after + "&limit=3&args[0]=1678831&args[1]=1678831";
     $http.get(url).success(function(data) {
       //console.log("Succesfully called API");
+      //console.log(this.after);
 
       /* Assigns the succesful JSON call to the variable items. */
       var items = data;
@@ -59,7 +63,7 @@ angular
       /* Assigns this.after to the amount of items in the array plus three. The feed pulls in
       three articles at a time. The next time it's run it will offset it by three to prevent
       the same articles from being pulled again. */
-      this.after = this.items.length + 3;
+      this.after = this.items.length;
       this.busy = false;
     }.bind(this));
   };
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,7 +13,7 @@ angular.module('obamaBlogApp')
     /* Creates a re-usable function that can be used to bind HTML to the DOM from
       the JSON. */
     $scope.to_trusted = function(html_code) {
-      return $sce.trustAsHtml(html_code)
+      return $sce.trustAsHtml(html_code);
     };
 
     /* Creates a new article object. Used alongside the Article factory. */
